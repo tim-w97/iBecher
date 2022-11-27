8b6a1ccb45de57171cdac9fb960c74ef7a2c55c4
Tim <timwagner997@gmail.com>
2022-11-27 23:51:24 +0100

    Initial commit

---
+++ iBecher/iBecherApp.swift
@@ -0,0 +1,17 @@
+//
+//  iBecherApp.swift
+//  iBecher
+//
+//  Created by Tim Wagner on 27.11.22.
+//
+
+import SwiftUI
+
+@main
+struct iBecherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
