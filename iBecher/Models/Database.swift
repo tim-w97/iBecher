401cc24e82ba0577f57bace2ffe388143de427fc
Tim <timwagner997@gmail.com>
2022-12-23 10:43:32 +0100

    put arview and toplid instances into the database

--- iBecher/Models/Database.swift
+++ iBecher/Models/Database.swift
@@ -6,13 +6,41 @@
 //
 
 import Foundation
+import RealityKit
 
-class Database {
+final class Database {
     static let sharedInstance = Database()
     
     var allCoffeePurchases: [CoffeePurchase]
     
+    let arView: ARView
+    let topLidScene: TopLid.Scene?
+    
     init() {
         allCoffeePurchases = []
+        
+        arView = ARView()
+        
+        // Load the Top Lid from the file
+        do {
+            let scene = try TopLid.loadScene()
+            topLidScene = scene
+        } catch {
+            print("Something went wrong while loading the Top Lid Scene:")
+            print(error)
+            
+            topLidScene = nil
+        }
+        
+        // Place the Top Lid on the ground
+        addTopLidToScene()
+    }
+    
+    private func addTopLidToScene() {
+        guard let topLidAnchor = topLidScene else {
+            return
+        }
+        
+        arView.scene.addAnchor(topLidAnchor)
     }
 }
--- iBecher/ViewModels/MugCheckerVM.swift
+++ iBecher/ViewModels/MugCheckerVM.swift
@@ -12,32 +12,6 @@ class MugCheckerVM : ObservableObject {
     let arView: ARView
     
     init() {
-        arView = ARView(
-            frame: .zero,
-            cameraMode: .ar,
-            automaticallyConfigureSession: true
-        )
-        
-        addTopLidToScene()
-    }
-    
-    private func addTopLidToScene() {
-        guard let topLidAnchor = loadTopLidScene() else {
-            return
-        }
-        
-        arView.scene.addAnchor(topLidAnchor)
-    }
-    
-    private func loadTopLidScene() -> TopLid.Scene? {
-        do {
-            let scene = try TopLid.loadScene()
-            return scene
-        } catch {
-            print("Something went wrong while loading the Top Lid Scene:")
-            print(error)
-        }
-        
-        return nil
+        arView = Database.sharedInstance.arView
     }
 }
