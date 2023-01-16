d241ddd331fadbecd3a2f7f7c3ebfdcec76ba01b
Tim <timwagner997@gmail.com>
2023-01-16 11:04:09 +0100

    added TimeInterval enum for the picker

---
+++ iBecher/Models/Types/TimeInterval.swift
@@ -0,0 +1,12 @@
+//
+//  TimeInterval.swift
+//  iBecher
+//
+//  Created by Tim Wagner on 16.01.23.
+//
+
+import Foundation
+
+enum TimeInterval {
+    case weekly, monthly, yearly, total
+}
--- iBecher/Screens/CoffeeConsumptionView.swift
+++ iBecher/Screens/CoffeeConsumptionView.swift
@@ -9,23 +9,27 @@ import SwiftUI
 
 struct CoffeeConsumptionView: View {
     @ObservedObject var vm: CoffeeConsumptionVM
-    @State var period = "Woche"
+    @State var period = TimeInterval.weekly
     
     var body: some View {
         VStack(spacing: 0) {
-            WeeklyCoffeePurchasesView(
-                purchases: vm.getWeeklyCoffeePurchases()
-            )
+            if period == .weekly {
+                WeeklyCoffeePurchasesView(
+                    purchases: vm.getWeeklyCoffeePurchases()
+                )
+            } else {
+                Spacer()
+            }
             
             Rectangle()
                 .fill(.blue)
                 .frame(height: 3)
             
             Picker("Zeitraum", selection: $period) {
-                Text("Wöchentlich")
-                Text("Monatlich")
-                Text("Jährlich")
-                Text("Insgesamt")
+                Text("Wöchentlich").tag(TimeInterval.weekly)
+                Text("Monatlich").tag(TimeInterval.monthly)
+                Text("Jährlich").tag(TimeInterval.yearly)
+                Text("Insgesamt").tag(TimeInterval.total)
             }.pickerStyle(.wheel)
         }
         .onAppear {
